e3aee285b2b9a4feb765a2206ab0df40cc14eb61
Ahmed M. <ahmmoussa22@gmail.com>
2026-01-30 10:51:06 -0500

    int32 changes

--- api/middleware.go
+++ api/middleware.go
@@ -20,7 +20,7 @@ func (m *Middleware) ValidateStagedFileExists(c *fiber.Ctx) error {
 	res, err := m.AtlasQueryClients.Storage.File(c.Context(), &req)
 	if err != nil {
 		return c.Status(400).SendString("Unable to find staged file with ID " + fileId)
-	} else if int64(len(res.File.Providers)) >= res.File.Replicas {
+	} else if len(res.File.Providers) >= res.File.Replicas {
 		return c.Status(409).SendString("File with ID " + fileId + "already has max. replicas stored accross the network.")
 	} else {
 		return c.Next()
